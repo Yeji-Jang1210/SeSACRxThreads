41873b419fa119a774e6760f0744b26bfb393060
Yeji-Jang1210 <jyg8727@gmail.com>
2024-08-03 22:42:09 +0900

    Initial Commit

---
+++ SeSACRxThreads/SignUp/PasswordViewController.swift
@@ -0,0 +1,86 @@
+//
+//  PasswordViewController.swift
+//  SeSACRxThreads
+//
+//  Created by jack on 2023/10/30.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+
+class PasswordViewController: UIViewController {
+   
+    let passwordTextField = SignTextField(placeholderText: "비밀번호를 입력해주세요")
+    let nextButton = PointButton(title: "다음")
+    let descriptionLabel = UILabel()
+    
+    let validText = Observable.just("8자리 이상 입력해주세요.")
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = Color.white
+        
+        configureLayout()
+        
+        bind()
+    }
+    
+    @objc func nextButtonClicked() {
+        navigationController?.pushViewController(PhoneViewController(), animated: true)
+    }
+    
+    func configureLayout() {
+        view.addSubview(passwordTextField)
+        view.addSubview(nextButton)
+        view.addSubview(descriptionLabel)
+         
+        passwordTextField.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.height.equalTo(20)
+            make.top.equalTo(passwordTextField.snp.bottom)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        nextButton.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(passwordTextField.snp.bottom).offset(30)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+    }
+    
+    private func bind(){
+        validText
+            .bind(to: descriptionLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        let validation = passwordTextField.rx.text.orEmpty
+                                .map{ $0.count >= 8 }
+        validation
+            .bind(to: nextButton.rx.isEnabled, descriptionLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        validation
+            .bind(with: self) { owner, value in
+                let color: UIColor = value ? .systemPink : .lightGray
+                owner.nextButton.backgroundColor = color
+            }
+            .disposed(by: disposeBag)
+        
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                print("show alert")
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    
+}
